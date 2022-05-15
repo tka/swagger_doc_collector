d8040469663349813064a72e822379ae5c1db3fd
tka <tkatka@gmail.com>
2022-05-15 19:35:09 +0800

    dont expand doc

--- downloader.go
+++ downloader.go
@@ -9,7 +9,6 @@ import (
 	"net/http"
 	"os"
 	"path"
-	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -103,58 +102,72 @@ func (downloader *Downloader) downloadFile(doc Doc) (err error) {
 		logger.Error("read body error ", zap.String("url", url), zap.Error(err))
 		return err
 	}
+	for i := 0; i < 1; i++ {
+		jsonRaw := json.RawMessage(body)
+		newSpec, err := loads.Analyzed(jsonRaw, "")
+		if err != nil {
+			logger.Error("analyzed json raw error", zap.String("url", url), zap.Error(err))
+			return err
+		}
+		/*
+			opt := spec.ExpandOptions{
+				AbsoluteCircularRef: false,
+			}
+			expandedDoc, err := newSpec.Expanded(&opt)
+			if err != nil {
+				logger.Error("expanded doc error", zap.String("url", url), zap.Error(err))
+				return err
+			}
+			expandedDocByte, err := json.MarshalIndent(expandedDoc.Spec(), "", "  ")
+			if err != nil {
+				logger.Error("marshal expanded doc error", zap.String("url", url), zap.Error(err))
+				return err
+			}
+			logger.Info("index", zap.Int("i", i), zap.Int("body", len(body)), zap.Int("len", len(expandedDocByte)))
+			//continue
+			// remove recursion $ref in doc
+			r, _ := regexp.Compile(`\$ref[^}]*`)
+			finalDoc := r.ReplaceAllString(string(expandedDocByte), "type\":\"object\"")
+			expandedDocByte = []byte(finalDoc)
+			newSpec, err = loads.Analyzed(expandedDocByte, "")
+			if err != nil {
+				return err
+			}*/
+		isSame, diffs, err := isSameLeastVersion(folder, newSpec)
+		if isSame {
+			logger.Info("same version", zap.String("url", url))
+			return nil
+		} else if err != nil {
+			// log and create new version
+			logger.Error("check api diff error", zap.String("url", url), zap.Error(err))
+			return err
 
-	jsonRaw := json.RawMessage(body)
-	newSpec, err := loads.Analyzed(jsonRaw, "")
-	if err != nil {
-		logger.Error("analyzed json raw error", zap.String("url", url), zap.Error(err))
-		return err
-	}
-	expandedDoc, err := newSpec.Expanded()
-	if err != nil {
-		logger.Error("expanded doc error", zap.String("url", url), zap.Error(err))
-		return err
-	}
-	expandedDocByte, err := json.MarshalIndent(expandedDoc.Spec(), "", "  ")
-	if err != nil {
-		logger.Error("marshal expanded doc error", zap.String("url", url), zap.Error(err))
-		return err
-	}
-	// remove recursion $ref in doc
-	r, _ := regexp.Compile(`\$ref[^}]*`)
-	finalDoc := r.ReplaceAllString(string(expandedDocByte), "type\":\"object\"")
-	expandedDocByte = []byte(finalDoc)
-	newSpec, err = loads.Analyzed(expandedDocByte, "")
-	if err != nil {
-		return err
-	}
-	isSame, diffs, err := isSameLeastVersion(folder, newSpec)
-	if isSame {
-		logger.Info("same version", zap.String("url", url))
-		return err
-	} else if err != nil {
-		// log and create new version
-		logger.Error("check api diff error", zap.String("url", url), zap.Error(err))
-	}
+		}
 
-	// Create the file
-	out, err := os.Create(filepath)
-	if err != nil {
-		logger.Error("create file path fail", zap.String("path", filepath), zap.Error(err))
-		return err
-	}
-	defer out.Close()
+		// Create the file
+		out, err := os.Create(filepath)
+		if err != nil {
+			logger.Error("create file path fail", zap.String("path", filepath), zap.Error(err))
+			return err
+		}
+		defer out.Close()
+		newSpecByte, err := json.MarshalIndent(newSpec.Spec(), "", "  ")
+		if err != nil {
+			logger.Error("marshal newSpec doc error", zap.String("url", url), zap.Error(err))
+			return err
+		}
+		// Writer the body to file
+		_, err = out.Write(newSpecByte)
+		if err != nil {
+			logger.Error("write file error", zap.String("url", url), zap.String("path", filepath), zap.Error(err))
+			return err
+		} else {
+			logger.Info("write file success", zap.String("url", url), zap.String("path", filepath))
+		}
 
-	// Writer the body to file
-	_, err = out.Write(expandedDocByte)
-	if err != nil {
-		logger.Error("write file error", zap.String("url", url), zap.String("path", filepath), zap.Error(err))
-		return err
-	} else {
-		logger.Info("write file success", zap.String("url", url), zap.String("path", filepath))
+		return SendSlackNotification(doc, diffs)
 	}
-
-	return SendSlackNotification(doc, diffs)
+	return nil
 }
 
 type SlackRequestBody struct {
@@ -182,6 +195,9 @@ func SendSlackNotification(doc Doc, diffs *diff.SpecDifferences) error {
 	return sendSlackMessage(msg)
 }
 func sendSlackMessage(msg string) error {
+	if config.SlackWebhookUrl == "" {
+		return nil
+	}
 	slackBody, _ := json.Marshal(SlackRequestBody{Text: msg})
 	req, err := http.NewRequest(http.MethodPost, config.SlackWebhookUrl, bytes.NewBuffer(slackBody))
 	if err != nil {
